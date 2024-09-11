397ca4bba80fd1536d7575b5b7548f39ba64c0ae
Nataly8674 <nataliacastilho26@gmail.com>
2024-09-11 19:01:15 -0300

    feat: inicializando o projeto

---
+++ src/main/java/com/unit/seduc/seducapp/SeducappApplication.java
@@ -0,0 +1,13 @@
+package com.unit.seduc.seducapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeducappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeducappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unit/seduc/seducapp/SeducappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unit.seduc.seducapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeducappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
